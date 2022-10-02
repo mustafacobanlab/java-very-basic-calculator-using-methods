781279ec57feb268c86b5e89821b33ae10a18c5c
Mustafa Çoban <mustafacobantr06@gmail.com>
2022-10-02 21:33:01 +0300

    class eklendi

---
+++ src/HesapMakinesi.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class HesapMakinesi {
+	public void islem() {
+		System.out.println("****Dort Islem Hesap Makinesi***");
+		Scanner scanner = new Scanner(System.in);
+				System.out.print("Sayi giriniz : ");
+				int a = scanner.nextInt();
+				System.out.print("Sayi giriniz : ");
+				int b = scanner.nextInt();
+				System.out.println("****Islem Turu Giriniz.****\n"+
+				"Toplama (T)\n"+"Cikarma (C)\n"+"Carpma (X)\n"+"Bolme (B)");
+				String Tur = scanner.nextLine();
+				Tur = scanner.nextLine();
+				
+				System.out.println("Sonuc:"+sayi(a,b,Tur));
+		
+
+	}
+   static int sayi(int a,int b,String Tur) {
+	   int s = 0;
+	   if(Tur.equals("T"))
+		 s =  a + b;
+	   else if(Tur.equals("C"))
+			 s =  a - b;
+	   else if(Tur.equals("X"))
+			 s =  a * b;
+	   else if(Tur.equals("B"))
+			 s =  a / b;
+	   else {
+		 JOptionPane.showMessageDialog(null,"Hatali islem turu girdiniz.");
+	}
+	   	   	return s;
+	}
+}
